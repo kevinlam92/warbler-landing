f97a60c729bf22e2ceee55a7ced6c9d7ae2c6016
Kevin <kevin@pokeyokey.com>
2022-03-13 00:52:06 -0800

    force dark

--- src/theme/palette.js
+++ src/theme/palette.js
@@ -1,32 +1,36 @@
 export const light = {
   alternate: {
     main: "#fdc82a",
-    dark: "#232323",
+    dark: "#fdc82a",
   },
-  cardShadow: "rgba(23, 70, 161, .11)",
-  mode: "light",
+  cardShadow: "rgba(0, 0, 0, .11)",
+  common: {
+    black: "#000",
+    white: "#fff",
+  },
+  mode: "dark",
   primary: {
-    main: "#377dff",
-    light: "#467de3",
-    dark: "#2f6ad9",
+    main: "#1e2022",
+    light: "#fff",
+    dark: "#000",
     contrastText: "#fff",
   },
   secondary: {
-    light: "#ffb74d",
-    main: "#f9b934",
-    dark: "#FF9800",
+    light: "#DBBE74",
+    main: "#cca43b",
+    dark: "#977727",
     contrastText: "rgba(0, 0, 0, 0.87)",
   },
   text: {
     primary: "#1e2022",
-    secondary: "#677788",
+    secondary: "#1e2022",
   },
-  divider: "rgba(0, 0, 0, 0.12)",
+  divider: "rgba(255, 255, 255, 0.12)",
   background: {
-    paper: "#ffffff",
-    default: "#ffffff",
-    level2: "#f5f5f5",
-    level1: "#ffffff",
+    paper: "#fdc82a",
+    default: "#fdc82a ",
+    level2: "#333",
+    level1: "#2D3748",
   },
 };
 
